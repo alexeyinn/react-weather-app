bdffb05ee82f71b62b91135bbe772a49da3fe21d
alexeyinn <alexey.i.nn@gmail.com>
2021-04-28 22:20:59 +0300

    feat: Названия городов автоматов с большой буквы

--- src/components/InputAdd.jsx
+++ src/components/InputAdd.jsx
@@ -4,8 +4,17 @@ export default React.memo(function InputAdd(props) {
 const [inputData, setInputData] = useState("");
 
 const cityAdd = () => {
+  function titleCase(str) {
+    let splitStr = str.toLowerCase().split(' ');
+    for (let i = 0; i < splitStr.length; i++) {
+        splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);     
+    }
+    return splitStr.join(' '); 
+ }
+const niceViwedData = titleCase(inputData);
+
   if (inputData !== '') {
-    props.setCustomCity([...props.customCity, inputData]);
+    props.setCustomCity([...props.customCity, niceViwedData]);
     setInputData('');
   } else {
     alert('Название не может быть пустым! Введите корректное название города!')
